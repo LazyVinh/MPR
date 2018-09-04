4468c8603936742428d0dcf177bd92d7bc383387
MA <xiuhloan@hotmail.de>
2018-09-04 22:21:45 +0200

    Korrektur x_lb und xub (war vertauscht)

--- berechneObereZustandsgrenze.m
+++ berechneObereZustandsgrenze.m
@@ -1,11 +1,11 @@
-function x_lb_vec = berechneObereZustandsgrenze(v_des, beta_lim, N2)
+function x_ub_vec = berechneObereZustandsgrenze(v_0, beta_lim, N2)
 
-x_lb_vec = zeros(3*N2,1);
-x_lb = [v_des; -beta_lim; -10];
+x_ub_vec = zeros(3*N2,1);
+x_ub = [v_0; beta_lim; 10];
 
     for i = 1:N2
          zeilen = (3*(i-1)+1);
-        x_lb_vec(zeilen:(zeilen+2),1) = x_lb;
+        x_ub_vec(zeilen:(zeilen+2),1) = x_ub;
     end
 
 end
--- berechneUntereZustandsgrenze.m
+++ berechneUntereZustandsgrenze.m
@@ -1,11 +1,11 @@
-function x_ub_vec = berechneUntereZustandsgrenze(v_0, beta_lim, N2)
+function x_lb_vec = berechneUntereZustandsgrenze(v_des, beta_lim, N2)
 
-x_ub_vec = zeros(3*N2,1);
-x_ub = [v_0; beta_lim; 10];
+x_lb_vec = zeros(3*N2,1);
+x_lb = [v_des; -beta_lim; -10];
 
     for i = 1:N2
          zeilen = (3*(i-1)+1);
-        x_ub_vec(zeilen:(zeilen+2),1) = x_ub;
+        x_lb_vec(zeilen:(zeilen+2),1) = x_lb;
     end
 
 end
