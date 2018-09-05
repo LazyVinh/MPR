b8b545e773b961b7d80b339cb7ad5d5fa976b763
MA <xiuhloan@hotmail.de>
2018-09-05 23:52:28 +0200

    NaN Werte zu Null deklariert.Und individuelle Gewichtung der Matrix Q.

--- berechneKostenfunktion.m
+++ berechneKostenfunktion.m
@@ -1,7 +1,7 @@
 %
 %   siehe Skript, Seite 341 H, Formel (10.15)
 %
-function J = berechneKostenfunktion(f, Phi, du, r, lambda)
+function J = berechneKostenfunktion(f, Phi, du, r, lambda, mue, gamma)
  
     % ueberpruefe Dimensionen
     [anzahlZeilen, anzahlSpalten] = size(Phi);
@@ -10,17 +10,28 @@ function J = berechneKostenfunktion(f, Phi, du, r, lambda)
         error("f hat falsche Dimensionen!")
     end
  
-    if any(size(r) ~= [anzahlZeilen, 1]);
+    if any(size(r) ~= [anzahlZeilen, 1])
         error("r hat falsche Dimensionen!")
     end
  
-    if any(size(du) ~= [anzahlSpalten, 1]);
+    if any(size(du) ~= [anzahlSpalten, 1])
         error("du hat falsche Dimensionen!")
     end
  
     % Q kann man in den Berechnungen eigntl weglassen,
     % weil Einheitsmatrix
-    Q = eye(anzahlZeilen, anzahlZeilen);
+    % Q = eye(anzahlZeilen, anzahlZeilen);
+    Q = zeros(anzahlZeilen, anzahlZeilen);
+    q = zeros(1,1);
+    
+    for i = 1:anzahlZeilen
+        if rem(i,2)
+            q(i) = gamma;
+        else
+            q(i) = mue;
+        end
+    end
+    Q = diag(q);
  
     % R wird eigntl auch nicht benoetigt, einfach lambda
     % multiplizieren ist auch ok (sogar effizienter!)
@@ -33,6 +44,12 @@ function J = berechneKostenfunktion(f, Phi, du, r, lambda)
     summand_2 = 2 * (e' * Q) * (Phi * du);
     summand_3 = du' * (Phi' * Q * Phi + R) * du;
  
-    J = summand_1 + summand_2 + summand_3;
+    Q = isnan(summand_1 + summand_2 + summand_3);
+    
+    if Q ==true
+        J = 0;
+    else
+        J = summand_1 + summand_2 + summand_3;
+    end
  
 end
